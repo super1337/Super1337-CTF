5a6023aeef2e95193628d2d6008d9a6674516449
Ayush <patel.ayush08@gmail.com>
2017-06-20 04:18:38 +0530

    Added mcqs view and template foundation

--- webapp-django/questionnaire/views.py
+++ webapp-django/questionnaire/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Question, MultipleChoiceQuestion
 
 def mcq(request):
-    return render(request,'questionnaire/mcq.html',{})
+    questions=MultipleChoiceQuestion.objects.all()
+    return render(request,'questionnaire/mcq.html',{'questions':questions})
