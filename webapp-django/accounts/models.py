18bd5e9a1ce6dd3d11bb2b153a711c46dbdf85e5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-19 11:19:38 +0530

    Add UserProfile model

--- webapp-django/accounts/models.py
+++ webapp-django/accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    bio = models.TextField(max_length=256, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+# Method to link the User and UserProfile models
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
