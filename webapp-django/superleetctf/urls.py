008b441cee47c830624cef39048cf1bd68ffb80d
Ayush <patel.ayush08@gmail.com>
2017-06-20 04:02:20 +0530

    questionnaire url setup

--- webapp-django/superleetctf/urls.py
+++ webapp-django/superleetctf/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'',include('questionnaire.urls'))
 ]
