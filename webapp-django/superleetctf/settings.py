606c4dc2fa827ddc26b1f8286a74d216d7c1aa6a
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-19 11:38:23 +0530

    Install django-allauth

--- webapp-django/superleetctf/settings.py
+++ webapp-django/superleetctf/settings.py
@@ -35,8 +35,17 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+    'questionnaire',
+    'jeopardyctf'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +107,19 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Authentication Backends
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
 
 
 # Internationalization
