37fd6d9b7a7d24d60f878d40916b28252637bca1
Ayush <patel.ayush08@gmail.com>
2017-06-20 05:10:14 +0530

    Template ready

--- webapp-django/questionnaire/views.py
+++ webapp-django/questionnaire/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 from .models import Question, MultipleChoiceQuestion
 
 def mcq(request):
-    questions=MultipleChoiceQuestion.objects.all()
+    que=MultipleChoiceQuestion.objects.all()
+    questions=[]
+    p=0
+    for h in que:
+        p+=1
+        q=0
+        qu = [h['question']]
+        ch=[]
+        for i in h.choices:
+            q+=1
+            ch+=[[q,i]]
+        qu+=[[ch]]
+        questions+=[qu]
+
     return render(request,'questionnaire/mcq.html',{'questions':questions})
