3aaceebe4f14515925c1b69a1dc4f2feb8946d35
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-19 10:04:58 +0530

    Add models for questions

--- webapp-django/questionnaire/models.py
+++ webapp-django/questionnaire/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_length=256)
+    hints = models.CharField(max_length=256, blank=True)
+    answer = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.question)
+
+
+class MultipleChoiceQuestion(Question):
+    choices = models.CharField(choices=[])
+    correct = models.IntegerField()
+
+    @classmethod
+    def create(cls, choices, correct):
+        CHOICES = [(index, item) for index, item in enumerate(choices)]
+        answer = CHOICES[correct][1]
+        mcq = cls(choices=CHOICES, correct=correct, answer=answer)
+        return mcq
