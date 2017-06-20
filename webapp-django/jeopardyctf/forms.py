0f9af4761d733cbfe0fd90e83b54c410a619caca
Ayush <patel.ayush08@gmail.com>
2017-06-21 00:33:41 +0530

    Added Form

---
+++ webapp-django/jeopardyctf/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from uploads.core.models import Document
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = ('description', 'document', )
