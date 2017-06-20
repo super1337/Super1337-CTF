58e204d7f428d6ecc9f1e44b717d3bc7112a5dbd
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-20 11:09:33 +0530

    Create dummy index fn. Reformat code.

--- webapp-django/questionnaire/views.py
+++ webapp-django/questionnaire/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+
 from .models import Question, MultipleChoiceQuestion
+
+
+def index(request):
+    pass
+
 
 def questions(request):
     ques = MultipleChoiceQuestion.objects.all() + Question.objects.all()
-    questions=[]
+    questions = []
 
-    p=0
+    p = 0
     for h in que:
-        p+=1
-        q=0
+        p += 1
+        q = 0
         qu = [h['question']]
-        ch=[]
+        ch = []
         for i in h.choices:
-            q+=1
-            ch+=[[q,i]]
-        qu+=[[ch]]
-        questions+=[qu]
+            q += 1
+            ch += [[q, i]]
+        qu += [[ch]]
+        questions += [qu]
 
-    return render(request, 'questions.html', {'questions':questions})
+    return render(request, 'questions.html', {'questions': questions})
