372e478bf8e799f0efa440f6a54a198427b7a16f
Ayush <patel.ayush08@gmail.com>
2017-06-21 00:21:18 +0530

    File Serving

--- webapp-django/superleetctf/urls.py
+++ webapp-django/superleetctf/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
