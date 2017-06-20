ee0230fec67978d95e2b7b5bf8f8fc2f02262c58
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-20 19:25:40 +0530

    Create models for jeopardyctf

--- webapp-django/jeopardyctf/models.py
+++ webapp-django/jeopardyctf/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+    problem = models.CharField(max_length=256)
+    hints = models.CharField(max_length=256, blank=True)
+    tags = models.ManyToManyField(Tag)
+    flag = models.CharField(max_length=256)
+
+    def __str__(self):
+        return str(self.problem)
