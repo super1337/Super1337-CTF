6a44d5bd0321cde2efffd9c33b764340640f2c9d
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-20 20:13:31 +0530

    Updated models

--- webapp-django/jeopardyctf/models.py
+++ webapp-django/jeopardyctf/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Tag(models.Model):
@@ -14,7 +16,20 @@ class Challenge(models.Model):
     problem = models.CharField(max_length=256)
     hints = models.CharField(max_length=256, blank=True)
     tags = models.ManyToManyField(Tag)
+    score = models.IntegerField()
+    creators = models.ManyToManyField(User)
     flag = models.CharField(max_length=256)
+
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        '''On save, update timestamps '''
+
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(User, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.problem)
--- webapp-django/questionnaire/models.py
+++ webapp-django/questionnaire/models.py
@@ -1,10 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return str(self.name)
 
 
 class Question(models.Model):
     question = models.CharField(max_length=256)
     hints = models.CharField(max_length=256, blank=True)
     answer = models.CharField(max_length=256)
+    tags = models.ManyToManyField(Tag)
+    score = models.IntegerField()
+    creators = models.ManyToManyField(User)
+
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        '''On save, update timestamps '''
+
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(User, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.question)
@@ -19,8 +43,8 @@ class MultipleChoiceQuestion(Question):
     correct = models.IntegerField()
 
     @classmethod
-    def create(cls, question, hints, choices, correct):
+    def create(cls, question, hints, choices, correct, score):
         CHOICES = [(index, item) for index, item in enumerate(choices)]
         answer = CHOICES[correct][1]
-        mcq = cls(question=question, hints=hints, choices=CHOICES, correct=correct, answer=answer)
+        mcq = cls(question=question, hints=hints, choices=CHOICES, correct=correct, answer=answer, score=score)
         return mcq
