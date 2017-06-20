f3d6e3299376113fdeb903d6ef1f39aa3e560e2a
Ayush <patel.ayush08@gmail.com>
2017-06-21 01:40:24 +0530

    Completed Questionnaire Form

--- webapp-django/questionnaire/forms.py
+++ webapp-django/questionnaire/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Questions
+from .models import Questions,SimpleQuestion,MultipleChoiceQuestion
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('question','hints','answer' )
+
+class SimpleQuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('question','hints','answer' )
+
+class MultipleChoiceQuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('question','hints','answer','choices','correct' )
