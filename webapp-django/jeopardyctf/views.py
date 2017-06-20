000eeb5dcf2841d5969aff862744c3c4b69fe855
Ayush <patel.ayush08@gmail.com>
2017-06-21 00:27:08 +0530

    Jeopardy Views upload

--- webapp-django/jeopardyctf/views.py
+++ webapp-django/jeopardyctf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
@@ -7,3 +8,14 @@ def index(request):
 
 def upload(request):
     return render(request,'jeopardyctf/upload.html',{})
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
