9087ddcba2853e92aa0cd42d6ae4a919bb538837
Ayush <patel.ayush08@gmail.com>
2017-06-21 22:26:12 +0530

    Fiexd challenges issues due to commenting

--- webapp-django/challenges/admin.py
+++ webapp-django/challenges/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Tag, Challenge
+from .models import Tag, Challenge, Document
 
-admin.site.register([Tag, Challenge])
+admin.site.register([Tag, Challenge, Document])
