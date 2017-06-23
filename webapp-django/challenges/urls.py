25984efedce97641b7bfc9469a3f0b8352c9bfcf
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-23 14:49:26 +0530

    Fixed challenges view.

--- webapp-django/challenges/urls.py
+++ webapp-django/challenges/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
+    url(r'^([a-z0-9.-]+)/$', views.challenge, name='challenge'),
     url(r'textBased/', views.textBased, name='textBased')
 ]
--- webapp-django/challenges/views.py
+++ webapp-django/challenges/views.py
@@ -10,18 +10,30 @@ def download(req):
     return response
 
 
+def textBased(request):
+    challenges = Challenge.objects.all()
+    return render(request, 'challenges/textBased.html', {'challenges': challenges})
+
+
 def index(request):
     challenges = Challenge.objects.all()
     return render(request, 'challenges/index.html', {'challenges': challenges})
+
+
+def challenge(request, name):
+    chal = Challenge.objects.get(name=name)
+    print(chal)
+    return render(request, 'challenges/challenge.html', {'challenge': chal})
+
 
     '''
     path=settings.MEDIA_ROOT
     file_list =os.listdir(path)
     return render(request,'challenges/index.html', {'files': file_list})
-    '''
+    
 
 
-'''
+
 def upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
@@ -45,8 +57,3 @@ def upload2(request):
         'form': form
     })
 '''
-
-
-def textBased(request):
-    challenges = Challenge.objects.all()
-    return render(request, 'challenges/textBased.html', {'challenges': challenges})
