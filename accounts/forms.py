9ec36e3d3e2af607b9d4e965194b795817d89607
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-25 02:38:21 +0530

    Used widget-tweak in edit profile page

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,3 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['bio']
-
-    def __init__(self, *args, **kwargs):
-        super(UserProfileForm, self).__init__(*args, **kwargs)
-        self.fields['bio'].widget.attrs['class'] = 'form-control'
