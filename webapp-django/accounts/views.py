a2cea688f122e195995a185d9516d1a01b56207e
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-06-24 12:20:02 +0530

    Added solved challenges page

--- webapp-django/accounts/views.py
+++ webapp-django/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
-from .models import UserProfile
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='/accounts/login/')
@@ -11,8 +12,25 @@ def index(request):
 
 
 def profile(request, username):
-    return render(request, 'accounts/profile.html', {'profile': request.user.userprofile})
+    messages = {'success': [], 'info': [], 'warning': [], 'danger': []}
+
+    try:
+        user = User.objects.get(username=username)
+    except ObjectDoesNotExist:
+        messages['warning'].append('The user {} does not exist!'.format(username))
+
+    return render(request, 'accounts/profile.html', {'profile': request.user.userprofile, 'messages': messages})
 
 
-def solved(request):
-    pass
+def solved(request, username):
+    messages = {'success': [], 'info': [], 'warning': [], 'danger': []}
+
+    try:
+        user = User.objects.get(username=username)
+    except ObjectDoesNotExist:
+        messages['danger'].append('The user {} does not exist!'.format(username))
+        solvedchals = []
+    else:
+        solvedchals = user.userprofile.solved_challenges.all()
+
+    return render(request, 'accounts/solved.html', {'solvedchals': solvedchals, 'messages': messages})
