e57ef2b56bbfca7e222407f12761445f03593fe4
yash98 <yashmalviya98@gmail.com>
2017-07-08 23:46:18 +0530

    added contest state 1 template and view

--- contests/views.py
+++ contests/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contest
 
 # Create your views here.
@@ -10,3 +10,9 @@ def index(request):
     ongoing = contests.filter(state=2)
     ended = contests.filter(state=3)
     return render(request, 'contests/index.html', {'yet_to_begin': yet_to_begin, 'ongoing': ongoing, 'ended': ended})
+
+
+def contest_view(request, name):
+    contest = get_object_or_404(Contest, name=name)
+    if contest.state == 1:
+        return render(request, 'contests/contest_1.html', {'contest': contest})
