08067cf9a9973154105f4d1333fa3aa7d44e5411
Ayush <patel.ayush08@gmail.com>
2017-07-10 19:25:48 +0530

    Added countdown

--- superleetctf/settings.py
+++ superleetctf/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import datetime
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -52,7 +54,8 @@ INSTALLED_APPS = [
     'accounts',
     'questionnaire',
     'challenges',
-    'contests'
+    'contests',
+    'countdown'
 ]
 
 MIDDLEWARE = [
@@ -63,7 +66,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'countdown.middleware.CountdownMiddleware',
 ]
+
 
 ROOT_URLCONF = 'superleetctf.urls'
 
@@ -171,3 +176,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # django-el-pagination settings
 
 EL_PAGINATION_PER_PAGE = 10
+
+
+COUNTDOWN_TARGET_DATE = datetime.datetime(2015, 1, 1, 0, 0, 0, 0)
