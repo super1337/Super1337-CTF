320f82c699340f87edd51c8ace6e0768e90c703b
yash98 <yashmalviya98@gmail.com>
2017-07-17 16:48:38 +0530

    Added slug freindly urls to challenges and contests

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^tags/$', views.tags, name='tags'),
-    url(r'^(?P<challenge_name>[a-z0-9.-]+)/$', views.challenge, name='challenge')
+    url(r'^(?P<challenge_slug>[a-z0-9.-]+)/$', views.challenge, name='challenge')
 ]
