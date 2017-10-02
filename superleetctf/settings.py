c58566dc1cba259fd24fa937343494304f6119a4
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-03 05:08:00 +0530

    staticfiles without the slashes

--- superleetctf/settings.py
+++ superleetctf/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
