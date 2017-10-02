22d35fc95417acf23b341bcad1aef9280bbecbe2
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-03 04:49:21 +0530

    Add whitenoise

--- superleetctf/settings.py
+++ superleetctf/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
-# import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -61,13 +61,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'countdown.middleware.CountdownMiddleware',
 ]
 
 
@@ -102,6 +102,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -176,5 +181,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EL_PAGINATION_PER_PAGE = 10
 
-
-# COUNTDOWN_TARGET_DATE = datetime.datetime(2015, 1, 1, 0, 0, 0, 0)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
